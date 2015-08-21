d0c9dd15ce4893b17e799db1d2cdfba8a84f60d5
HIDEKUNI Kajita <hide.nba@gmail.com>
2015-08-22 00:04:14 +0900

    refacter

--- lib/swing_servo.rb
+++ lib/swing_servo.rb
@@ -5,6 +5,8 @@ module SwingServo
   class Controller
     attr_reader :board, :yaw, :pitch, :yaw_pin, :pitch_pin
 
+    RESET_POSITION = 90
+
     def initialize(yaw_pin, pitch_pin)
       @yaw_pin = yaw_pin
       @pitch_pin = pitch_pin
@@ -25,8 +27,8 @@ module SwingServo
       @yaw = Dino::Components::Servo.new(pin: yaw_pin, board: board)
       @pitch = Dino::Components::Servo.new(pin: pitch_pin, board: board)
 
-      swing(yaw, 0)
-      swing(pitch, 0)
+      swing(yaw, RESET_POSITION)
+      swing(pitch, RESET_POSITION)
     end
 
     private
